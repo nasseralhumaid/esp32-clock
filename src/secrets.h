776f2bce41786130eb1c83e068fbfda33a8c97ec
Nasser <nasseralhumaid@pm.me>
2026-09-22 18:11:14 +0100

    Add example secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+//Edit this with your real credentials!
+
+#define SECRET_SSID "SSID       "
+#define SECRET_PW "PASSWORD"
+#define OPENWEATHERMAPKEY "OPENWEATHERMAPAPIKEY"
+
+#define LATITUDE  "LATITUDE"
+#define LONGDITUDE "LONGDITUDE"
