506c94d4416121b10cdf2bba3f93cc4be18ba8b9
Nasser <nasseralhumaid@pm.me>
2026-09-22 18:16:59 +0100

    Fix formatting of SECRET_SSID in secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -1,6 +1,6 @@
 //Edit this with your real credentials!
 
-#define SECRET_SSID "SSID       "
+#define SECRET_SSID "SSID"
 #define SECRET_PW "PASSWORD"
 #define OPENWEATHERMAPKEY "OPENWEATHERMAPAPIKEY"
 
